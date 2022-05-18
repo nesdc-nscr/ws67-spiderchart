854db7ced6f5e34ca64f04e5ba796f0dca018cdc
ahfyks <dujfha@gmail.com>
2022-05-18 23:16:29 +0700

    edit font to kanit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+import { Global, MantineProvider } from "@mantine/core";
 
 
 export default function App(props: AppProps) {
@@ -9,13 +9,26 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+      <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet"/>
         <title>Spider Chart หลักเกณฑ์การประเมินโครงการฯ</title>
         <meta
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width'
         />
       </Head>
-
+      {/* <Global
+      styles={[
+        {
+          '@font-face': {
+            fontFamily: 'Kanit',
+            src: `url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap')`,
+            fontStyle: 'normal',
+          },
+        },
+      ]}
+      /> */}
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
@@ -29,10 +42,10 @@ export default function App(props: AppProps) {
             md: 1000,
             lg: 1200,
             xl: 1400,
-          },
+          },         
         }}>
         <Component {...pageProps} />
       </MantineProvider>
-    </>
+    </> 
   );
 };
